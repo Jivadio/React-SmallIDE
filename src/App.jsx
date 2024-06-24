da1455d2f9566b4fcf5953b1c3f680e30d3be79f
Malo <42148894+Jivadio@users.noreply.github.com>
2024-06-24 15:48:44 +0200

    Add Tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import {AdjustmentsVerticalIcon} from "@heroicons/react/24/solid/index.js";
+import Tabs from "./components/Tabs";
 
 function App() {
     return (
@@ -6,7 +7,7 @@ function App() {
             <div className="p-4 border-b flex items-center">
                 <h1 className="w-full text-slate-50 text-2xl">React-<span className="text-xs">Small</span>IDE</h1>
                 <div className="w-full">
-                    <button>Show Preview</button>
+                    <button>Voir la preview</button>
                 </div>
                 <div className="w-full flex">
                     <div className="ml-auto mr-2">
@@ -14,6 +15,7 @@ function App() {
                     </div>
                 </div>
             </div>
+            <Tabs/>
         </div>
     )
 }
